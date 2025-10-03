831c7c1f3d086de41e7a88e24707d335744ec436
Plugo Employee <ligar@plugo.co>
2025-10-03 14:06:29 +0700

    Add .gitignore and remove .plasmo cache directory

--- .plasmo/process.env.d.ts
+++
@@ -1,9 +0,0 @@
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-		PLASMO_PUBLIC_SUPABASE_URL?: string
-		PLASMO_PUBLIC_SUPABASE_ANON_KEY?: string
-		PLASMO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY?: string
-		PLASMO_PUBLIC_RAJAONGKIR_API_KEY?: string
-  }
-}
